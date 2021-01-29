30f10a198345ecd4270510563b2ddba6c8dca892
Tao Luan <tvanluanst@gmail.com>
2021-01-29 23:32:41 +0700

    deloy-server

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(helmet())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use('/api',routesAPI)
 io.on('connection', (socket) => {
     socket.on('setName',(name)=>controllerSocket.setName(name,socket,io))
